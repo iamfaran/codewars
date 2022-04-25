b9039ee0c3a40bab9875d51fe0030e767ea5449c
Faran <faran@spotnpaste.net>
2022-04-25 05:49:37 +0500

    solve shortest word

--- script.js
+++ script.js
@@ -1,46 +1,20 @@
-// Complete the method/function so that it converts
-//dash/underscore delimited words into camel casing.
-//The first word within the output should be capitalized only
-//if the original word was capitalized
-//(known as Upper Camel Case, also often referred to as Pascal case).
-
 /* 
+bitcoin take over the world maybe who knows perhaps
 
-Examples
-1 -> "the-stealth-warrior" gets converted to "theStealthWarrior"
-2 ->  "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+return the lenght of shortest words in this example "3"
 
 */
 
-// function toCamelCase(str) {
-//   let firstCapital = false;
-//   let res = "";
-//   const strArr = str.split(/[-_]/);
-//   strArr.forEach((word, index) => {
-//     if (word.charAt() === word.charAt().toUpperCase()) {
-//       firstCapital = true;
-//     }
-//     if (firstCapital) {
-//       res += word.charAt().toUpperCase();
-//       res += word.slice(1);
-//       return;
-//     }
-//     if (index > 0) {
-//       res += word.charAt().toUpperCase();
-//       res += word.slice(1);
-//     }
-//   });
+function shortWords(str) {
+  const strArr = str.split(" ");
+  let shortestWord = strArr[0];
 
-//   console.log(res);
-// }
-
-function toCamelCase(str) {
-  debugger;
-  str = str.split(/[-_]/);
-  for (var i = 1; i < str.length; i++) {
-    console.log(str[i].charAt(0).toUpperCase().concat(str[i].slice(1)));
-    str[i] = str[i].charAt(0).toUpperCase().concat(str[i].slice(1));
-  }
-  return str.join("");
+  strArr.forEach((word) => {
+    if (word.length < shortestWord.length) {
+      shortestWord = word;
+    }
+  });
+  return shortestWord.length;
 }
-console.log(toCamelCase("the-stealth-warrior"));
+
+shortWords("bitcoin take over the world maybe who knows perhaps");
