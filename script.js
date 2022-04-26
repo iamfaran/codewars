8516f9f2b40e037c399c3cf3c4d5590dee22a572
Faran <faran@spotnpaste.net>
2022-04-26 05:43:08 +0500

    weird match length prop

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 /* 
-bitcoin take over the world maybe who knows perhaps
-
-return the lenght of shortest words in this example "3"
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
 
 */
 
-function shortWords(str) {
-  const strArr = str.split(" ");
-  let shortestWord = strArr[0];
-
-  strArr.forEach((word) => {
-    if (word.length < shortestWord.length) {
-      shortestWord = word;
+function countDuplicate(str) {
+  debugger;
+  let count = 0;
+  const strArr = str.split("");
+  const resArr = [];
+  strArr.forEach((letter) => {
+    const test = new RegExp(`${letter}`, "gi");
+    if (!resArr.includes(letter.toLowerCase())) {
+      if (str.match(test).length < 0);
+      {
+        console.log(typeof str.match(test));
+        console.log(typeof str.match(test).length);
+        count++;
+      }
+      resArr.push(letter.toLowerCase());
     }
   });
-  return shortestWord.length;
 }
 
-shortWords("bitcoin take over the world maybe who knows perhaps");
+countDuplicate("Indivisibilities");
